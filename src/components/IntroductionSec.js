c98d0aac179a6aec7a2807c2328aa4a57d750925
Wenhao <owengwh123@gmail.com>
2019-09-02 01:44:50 -0400

    Minor change

--- src/components/IntroductionSec.js
+++ src/components/IntroductionSec.js
@@ -10,7 +10,7 @@ class IntroductionSec extends Component {
         <div className="title">
 
           <h1 style={{ color: 'wheat', textAlign: "left", marginTop: '2vw' }}>
-            Wenhao Geng
+            Owen Geng
             <span role="img" aria-label="climbing">  🧗🏽‍♂️ </span>
           </h1>
 
