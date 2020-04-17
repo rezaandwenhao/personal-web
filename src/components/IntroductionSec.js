469317269b8c320c3bdbdde33ef42ac985456dd9
Wenhao <owengwh123@gmail.com>
2020-04-16 21:19:38 -0400

    Update info

--- src/components/IntroductionSec.js
+++ src/components/IntroductionSec.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Clock from '../Clock';
-import { Badge } from 'react-bootstrap';
+import React, { Component } from "react";
+import Clock from "../Clock";
+import { Badge } from "react-bootstrap";
 
 class IntroductionSec extends Component {
   render() {
@@ -8,51 +8,61 @@ class IntroductionSec extends Component {
       <div className="intro">
         <Clock />
         <div className="title">
-
-          <h1 style={{ color: 'wheat', textAlign: "left", marginTop: '2vw' }}>
+          <h1 style={{ color: "wheat", textAlign: "left", marginTop: "2vw" }}>
             Owen (Wenhao) Geng
           </h1>
 
-          <h3 style={{ color: 'white', textAlign: "left" }}>
-            Software Developer  |  Computer Engineering Student  |  Table Tennis Player
+          <h3 style={{ color: "white", textAlign: "left" }}>
+            Software Developer | Computer Engineering Student | Table Tennis
+            Player
           </h3>
 
-          <h4 style={{ color: 'white', textAlign: "left", marginTop: '3.5vw' }}>
-            Experienced with Java, frontend and mobile development. Open for software engineer internship opportunities.
+          <h4 style={{ color: "white", textAlign: "left", marginTop: "3.5vw" }}>
+            Experienced with Java, full-stack web development and mobile
+            development. Open for software engineer internship opportunities.
           </h4>
 
-          <h4 style={{ color: 'white', textAlign: "left" }}>
+          <h4 style={{ color: "white", textAlign: "left" }}>
             I work with:
-            <Badge pill variant="primary" style={{ marginLeft: '1vw' }}>
+            <Badge pill variant="primary" style={{ marginLeft: "1vw" }}>
               Java
             </Badge>
-            <Badge pill variant="secondary" style={{ marginLeft: '1vw' }}>
+            <Badge pill variant="secondary" style={{ marginLeft: "1vw" }}>
               Javascript
             </Badge>
-            <Badge pill variant="success" style={{ marginLeft: '1vw' }}>
-              C#
+            <Badge pill variant="success" style={{ marginLeft: "1vw" }}>
+              Node.js
             </Badge>
-            <Badge pill variant="info" style={{ marginLeft: '1vw' }}>
+            <Badge pill variant="info" style={{ marginLeft: "1vw" }}>
               React.js
             </Badge>
-            <Badge pill variant="light" style={{ marginLeft: '1vw' }}>
+            <Badge pill variant="light" style={{ marginLeft: "1vw" }}>
               Redux
             </Badge>
-            <Badge pill variant="danger" style={{ marginLeft: '1vw' }}>
+            <Badge pill variant="danger" style={{ marginLeft: "1vw" }}>
               Vue.js
             </Badge>
-            <Badge pill variant="warning" style={{ marginLeft: '1vw' }}>
+            <Badge pill variant="warning" style={{ marginLeft: "1vw" }}>
               Spring Framework
+            </Badge>
+            <Badge pill variant="primary" style={{ marginLeft: "1vw" }}>
+              MongoDB
             </Badge>
           </h4>
 
-          <h4 style={{ color: 'white', textAlign: "left", marginTop: '3.5vw' }}>
-            {"Expected graduation in December 2020 | "}
-            <a href="https://drive.google.com/open?id=108xPMDFBOOFovsR9bb_J6WThWDHKDqEY" target="_blank" rel="noopener noreferrer">View my resume</a>
+          <h4 style={{ color: "white", textAlign: "left", marginTop: "3.5vw" }}>
+            {"Expected graduation from McGill University in December 2020 | "}
+            <a
+              href="https://drive.google.com/open?id=108xPMDFBOOFovsR9bb_J6WThWDHKDqEY"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View my resume
+            </a>
           </h4>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/ProjectsSec.js
+++ src/components/ProjectsSec.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import GarbageLookup from '../resources/Garbage Lookup.gif';
-import Nventive from '../resources/Nventive.jpg';
-import MathUp from '../resources/MATH-UP.png';
-import Robot from '../resources/Robot.png';
-import Ericsson from '../resources/Ericsson.jpg';
-import TileO from '../resources/TileO.png';
-import Footer from './Footer';
+import React, { Component } from "react";
+import GarbageLookup from "../resources/Garbage Lookup.gif";
+import Nventive from "../resources/Nventive.jpg";
+import MathUp from "../resources/MATH-UP.png";
+import Robot from "../resources/Robot.png";
+import Ericsson from "../resources/Ericsson.jpg";
+import TileO from "../resources/TileO.png";
+import Footer from "./Footer";
 
 class ProjectsSec extends Component {
   render() {
@@ -13,77 +13,115 @@ class ProjectsSec extends Component {
       <div className="projects">
         <div id="project">
           <div id="aProject">
-            <a href="https://hidden-plateau-61052.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://hidden-plateau-61052.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img className="photo" src={GarbageLookup} alt="" />
             </a>
-            <p style={{ color: 'white', paddingTop: 10 }}>
-              Project  |  2019
-            </p>
-            <a href="https://hidden-plateau-61052.herokuapp.com/" target="_blank" rel="noopener noreferrer">Website</a>
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            <a href="https://github.com/rezaandwenhao/Gabage-Lookup-React" target="_blank" rel="noopener noreferrer">Github</a>
+            <p style={{ color: "white", paddingTop: 10 }}>Project | 2019</p>
+            <a
+              href="https://hidden-plateau-61052.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Website
+            </a>
+            &nbsp; &nbsp; &nbsp;
+            <a
+              href="https://github.com/rezaandwenhao/Gabage-Lookup-React"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
           </div>
         </div>
         <div id="project">
           <div id="aProject">
             <img className="photo" src={Nventive} alt="" />
-            <p style={{ color: 'white', paddingTop: 10 }}>
-              Nventive Internship  |  2019
+            <p style={{ color: "white", paddingTop: 10 }}>
+              Nventive Internship | 2019
             </p>
-            <p style={{ color: 'white' }}>Coming Soon!</p>
           </div>
         </div>
         <div id="project">
           <div id="aProject">
-            <a href="https://github.com/rezaandwenhao/MATH-UP" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/rezaandwenhao/MATH-UP"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img className="photo" src={MathUp} alt="" />
             </a>
-            <p style={{ color: 'white', paddingTop: 10 }}>
-              Project  |  2018
-            </p>
-            <a href="https://github.com/rezaandwenhao/MATH-UP" target="_blank" rel="noopener noreferrer">Github</a>
+            <p style={{ color: "white", paddingTop: 10 }}>Project | 2018</p>
+            <a
+              href="https://github.com/rezaandwenhao/MATH-UP"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
           </div>
         </div>
         <div id="project">
           <div id="aProject">
-            <a href="https://drive.google.com/open?id=1nxU33NAUv72MRqcaAAUUgR7n6xqlB8Og" target="_blank" rel="noopener noreferrer">
-              <img className="photo" src={Robot} alt=""/>
+            <a
+              href="https://drive.google.com/open?id=1nxU33NAUv72MRqcaAAUUgR7n6xqlB8Og"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="photo" src={Robot} alt="" />
             </a>
-            <p style={{ color: 'white', paddingTop: 10 }}>
-              Project  |  2018
-            </p>
-            <a href="https://drive.google.com/open?id=1nxU33NAUv72MRqcaAAUUgR7n6xqlB8Og" target="_blank" rel="noopener noreferrer">Demo</a>
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            <a href="https://github.com/rezaandwenhao/DPM-Robot-Competition" target="_blank" rel="noopener noreferrer">Github</a>
+            <p style={{ color: "white", paddingTop: 10 }}>Project | 2018</p>
+            <a
+              href="https://drive.google.com/open?id=1nxU33NAUv72MRqcaAAUUgR7n6xqlB8Og"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Demo
+            </a>
+            &nbsp; &nbsp; &nbsp;
+            <a
+              href="https://github.com/rezaandwenhao/DPM-Robot-Competition"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
           </div>
         </div>
         <div id="project">
           <div id="aProject">
             <img className="photo" src={Ericsson} alt="" />
-            <p style={{ color: 'white', paddingTop: 10 }}>
-              Ericsson Internship  |  2018
+            <p style={{ color: "white", paddingTop: 10 }}>
+              Ericsson Internship | 2018
             </p>
-            <p style={{ color: 'white' }}>Coming Soon!</p>
           </div>
         </div>
         <div id="project">
           <div id="aProject">
-            <a href="https://github.com/rezaandwenhao/TileO" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/rezaandwenhao/TileO"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img className="photo" src={TileO} alt="" />
             </a>
-            <p style={{ color: 'white', paddingTop: 10 }}>
-              Project  |  2017
-            </p>
-            <a href="https://github.com/rezaandwenhao/TileO" target="_blank" rel="noopener noreferrer">Github</a>
+            <p style={{ color: "white", paddingTop: 10 }}>Project | 2017</p>
+            <a
+              href="https://github.com/rezaandwenhao/TileO"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
           </div>
         </div>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
